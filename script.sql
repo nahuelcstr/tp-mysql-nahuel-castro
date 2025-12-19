fbf88505cd310523ead8663d707dac15749e5350
nahuelcstr <nahuel.castro.0ne@gmail.com>
2025-12-18 21:57:31 -0300

    creacion de tablas

--- script.sql
+++ script.sql
@@ -1,5 +1,7 @@
+DROP DATABASE IF EXISTS veterinaria_patitas_felices;
 CREATE DATABASE veterinaria_patitas_felices;
 USE veterinaria_patitas_felices;
+
 
 CREATE TABLE duenos (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -15,5 +17,23 @@ CREATE  TABLE mascotas(
     especie VARCHAR(50) NOT NULL,
     fecha_nacimiento DATE,
     id_dueno INT,
-    FOREING KEY (id_dueno) REFERENCES duenos(id) 
+    FOREIGN KEY (id_dueno) REFERENCES duenos(id) 
+);
+
+CREATE TABLE veterinarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    matricula VARCHAR(20) NOT NULL UNIQUE,
+    especialidad VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE historial_clinico (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_mascota INT,
+    id_veterinario INT,
+    fecha_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    descripcion VARCHAR(250) NOT NULL,
+    FOREIGN KEY (id_mascota) REFERENCES mascotas(id) ON DELETE CASCADE,
+    FOREIGN KEY (id_veterinario) REFERENCES veterinarios(id)
 );
